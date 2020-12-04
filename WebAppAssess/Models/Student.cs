0b34b92c3594d4f8afbf3c40a4ed96ee7e4f6673
rishabh817 <rishabhagarwal817@gmail.com>
2020-12-04 16:17:28 +0530

    second commit

---
+++ WebAppAssess/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppAssess.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public DateTime SDob { get; set; }
+    
+        public string SContact { get; set; }
+
+}
+}
